a6b4f02687d62ac8761c892fe335e4fcf2792a13
Darlan Alvysson Alves <darlan.alves@sp01.local>
2020-06-14 12:26:42 -0300

    =>Criação do componente home =>Criação do componente crud de produto => Configuração e entendimento de rotas de navegação

--- CRUD/frontend/src/app/app-routing.module.ts
+++ CRUD/frontend/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+   path:"", /*Vazio indica que é a rota raiz*/
+   component: HomeComponent /*Indica qual é o componente da rota raiz*/
+  },
+  {
+    path:"products", 
+    component: ProductCrudComponent /*Indica qual é o componente da rota*/
+   }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
